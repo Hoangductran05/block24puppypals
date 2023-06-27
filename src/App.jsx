468dd67a875a7e6c5fbab40040b13f0c6abbd8ef
Hoangductran05 <hoangducdoodle@gmail.com>
2023-06-27 10:37:00 -0500

    render

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
 import {puppyList} from './data.js'
 import { useState } from 'react'
 import './App.css'
+import './index.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
   console.log("puppyList: ", puppyList)
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+  function handleClose() {
+    setFeatPupId(null);
+  }
 
   return (
     <>
-      <div>
+      <div className='App'>
         {
            puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p 
+            className={`puppy ${puppy.id === featPupId ? 'featured' : ''}`} 
+            onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
         }
+        {featPupId && featuredPup && (
+          <div className='popup'>
+           
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+            <button className='close-button' onClick={handleClose}>Close</button>
+          </div>
+        )}
       </div>
      
-      <div className="card">
-       
-      </div>
+     
       
     </>
   )
