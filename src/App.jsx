4aad322a8f61a8c53509cebcc84f37941977120b
Hoangductran05 <hoangducdoodle@gmail.com>
2023-06-26 23:09:07 -0500

    delete space

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import {puppyList} from './data.js'
 import { useState } from 'react'
-
-
 import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-
-  
   console.log("puppyList: ", puppyList)
 
   return (
